0d4b794c3956906b80c0360e8e6072abc05a66fe
Revathi Narayankutty <revathynarayankutty@gmail.com>
2016-07-23 15:46:30 +0530

    Few simple string operations.

---
+++ StringOperations.py
@@ -0,0 +1,30 @@
+s="I am an Indian"
+print(s)
+
+s="Here is a line \n split into two lines"
+print(s)
+
+s=""" This is a,
+	multiline string, so you can
+	write in many lines """
+print (s)
+
+s= "revathy narayankutty"
+print (s.title())
+print(s.upper())
+print(s.lower())
+
+s="I am A pRoGraMMeR"
+print(s.swapcase())
+
+s="abcd 11234efg"
+print(s.isalnum())
+
+s="abcd11234efg"
+print(s.isalnum())
+
+s="We love Python"
+print(s.split(" "))
+
+s="-".join("GNU/Linux is great".split(" "))
+print(s)
